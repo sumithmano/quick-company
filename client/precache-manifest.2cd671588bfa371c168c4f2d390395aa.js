953702af3e76605c1e363df69efd06c522ff95e7
Sumith <sumith@Sumiths-MacBook-Pro.local>
2020-09-07 20:38:59 +0530

    Initial Commit

---
+++ client/precache-manifest.2cd671588bfa371c168c4f2d390395aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1576f225db23467eb89fc37c0e05779c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "336e1d0d46758c322055",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d60646b22831372c3994",
+    "url": "/static/js/2.db847a77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db847a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336e1d0d46758c322055",
+    "url": "/static/js/main.70cb8cef.chunk.js"
+  },
+  {
+    "revision": "9f2124692b25050f27c2",
+    "url": "/static/js/runtime-main.50b05a76.js"
+  }
+]);
