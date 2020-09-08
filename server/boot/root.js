ceb1372168ce740b2aa58468dbff592b7a10d8c7
Sumith <sumith@Sumiths-MacBook-Pro.local>
2020-09-08 09:41:02 +0530

    add models

--- server/boot/root.js
+++ server/boot/root.js
@@ -8,7 +8,8 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   const router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  // router.get('/');
+  // replace for testing
+  // router.get('/', server.loopback.status());
+  router.get('/');
   server.use(router);
 };
